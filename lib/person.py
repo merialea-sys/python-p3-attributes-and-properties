b2e7565e34b3e714a60499db6d3f9550735270f7
merialea <merialea5@gmail.com>
2025-11-14 21:44:56 +0300

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Bob",job = "doctor"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        is_valid = isinstance(name,str) and (1<= len(name) <=25)
+
+        if is_valid:
+            self._name = name.title()
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self,job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs." )
+
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
